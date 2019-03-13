5150f27888795524927d442baa6a8eb6849bbcec
jiaweniscute <ng.jia.wen@hotmail.com>
2019-03-13 15:21:24 -0400

    fixed errors

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid');
+var mymap = L.map('mapid').setView([51.524416, -0.134265], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
     { maxZoom: 18,
